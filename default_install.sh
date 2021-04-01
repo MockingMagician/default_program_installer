28f78b01dca3f2ba5bd51269f62a9eb269174070
Marc MOREAU <moreau.marc.web@gmail.com>
2021-04-02 00:53:10 +0200

    doc
    fix typos

--- default_install.sh
+++ default_install.sh
@@ -1,4 +1,4 @@
-#instal apt repositoring
+# install apt repositories
 password=$(zenity --password)
 echo $password | sudo -S apt upgrade && sudo apt install -y \
   xclip \
@@ -19,25 +19,25 @@ echo $password | sudo -S apt upgrade && sudo apt install -y \
   curl \
   wget
 
-#instal slack
-slack=$(curl https://slack.com/intl/fr-fr/downloads/instructions/ubuntu | grep -oE https://downloads.slack-edge.com/linux_releases/.+\.deb) #recup de l'url
-wget -O slack.deb $slack #dwnld du fichier
-sudo dpkg -i slack.deb #instal de slack
-unlink slack.deb #supp fichier instal
+# install slack
+slack=$(curl https://slack.com/intl/fr-fr/downloads/instructions/ubuntu | grep -oE https://downloads.slack-edge.com/linux_releases/.+\.deb) # fetch slack download url
+wget -O slack.deb $slack # downlod file
+sudo dpkg -i slack.deb # install
+unlink slack.deb # delete installer
 
-#instal owmyzsh
+# install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#instal anydesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - #ajouter cles repo
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list #ajoute repo
-sudo apt update #update apt cache
-sudo apt install anydesk #instal anydesk
+# install anydesk
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - # add repo key
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list # add repo
+sudo apt update # update apt cache
+sudo apt install anydesk # install anydesk
 
-#instal phpstorm
-wget -O jetbrains-toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.20.7940.tar.gz #telecharger l'installer
-sudo -S tar -xf jetbrains-toolbox.tar.gz -C /opt #extraire l'archive
-cd /opt/jetbrains-toolbox-1.20.7940 && ./jetbrains-toolbox #installer toolbox
-cd - #retour dans repertoire
-unlink jetbrains-toolbox.tar.gz #supp le fichier apres installation
-zenity --info --text="continuer l'installation dans tool box." --width=200 --height=100 #message pour la suite manuel
+# instal jetbrain toolbox
+wget -O jetbrains-toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.20.7940.tar.gz # download installer
+sudo -S tar -xf jetbrains-toolbox.tar.gz -C /opt # extract installer
+cd /opt/jetbrains-toolbox-1.20.7940 && ./jetbrains-toolbox # install toolbox
+cd - # go back
+unlink jetbrains-toolbox.tar.gz # delete installer
+zenity --info --text="continue to install program like phpstorm, etc... from the toolbox window." --width=200 --height=100 #message pour la suite manuel
