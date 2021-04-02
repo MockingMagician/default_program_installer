134f670d9ab6a7c2f1c59a214cbb02e8be5b32bd
Marc MOREAU <moreau.marc.web@gmail.com>
2021-04-02 21:00:33 +0200

    doc
    fix typos

--- default_install.sh
+++ default_install.sh
@@ -1,9 +1,12 @@
-# install apt repositories
+# friendly password
 password=$(zenity --password)
-echo $password | sudo -S apt upgrade && sudo apt install -y \
+
+# update password
+echo $password | sudo -S apt upgrade
+
+# install apt repositories
+sudo apt install -y \
   xclip \
-  git \
-  npm \
   terminator \
   zsh \
   docker.io \
@@ -34,10 +37,10 @@ echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-st
 sudo apt update # update apt cache
 sudo apt install anydesk # install anydesk
 
-# instal jetbrain toolbox
+# install jetbrain toolbox
 wget -O jetbrains-toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.20.7940.tar.gz # download installer
 sudo -S tar -xf jetbrains-toolbox.tar.gz -C /opt # extract installer
 cd /opt/jetbrains-toolbox-1.20.7940 && ./jetbrains-toolbox # install toolbox
 cd - # go back
 unlink jetbrains-toolbox.tar.gz # delete installer
-zenity --info --text="continue to install program like phpstorm, etc... from the toolbox window." --width=200 --height=100 #message pour la suite manuel
+zenity --info --text="continue to install program like phpstorm, etc... from the jetbrain toolbox window." --width=200 --height=100 # user message continue to manually install
